bdac57d9396109451af347995ea67d4b9161c8f0
屿lllllllll <3383455720@qq.com>
2026-03-14 01:32:46 +0800

    deploy: publish static site

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <MusicPlayer />
-</template>
-
-<script setup lang="ts">
-import MusicPlayer from './components/MusicPlayer.vue'
-</script>
--- src/utils/request.ts
+++
@@ -1,7 +0,0 @@
-export const request = async (url: string, options: RequestInit = {}) => {
-  const response = await fetch(url, options);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return await response.json();
-};
