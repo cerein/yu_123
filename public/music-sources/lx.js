a5a642b6d5bc71cc9b5b3f3915d444b9c7277479
屿lllllllll <3383455720@qq.com>
2026-03-15 04:19:30 +0800

    feat: 添加三个洛雪音乐音源到静态资源

---
+++ public/music-sources/lx.js
@@ -0,0 +1,8 @@
+/*!
+ * @name [独家音源]
+ * @description 音源更新，关注微信公众号：洛雪科技
+ * @version 4
+ * @author 洛雪科技
+ * @repository https://github.com/lxmusics/lx-music-api-server
+ */
+// 内容来自 https://raw.githubusercontent.com/pdone/lx-music-source/main/lx/latest.js
---
+++ public/music-sources/sixyin.js
@@ -0,0 +1,11 @@
+/*!
+ * @name 六音音源
+ * @description v1.2.1 如失效请前往 www.sixyin.com 下载最新版本
+ * @version v1.2.1
+ * @author 六音
+ * @homepage www.sixyin.com
+ * @netease MUSIC_U=;
+ * @tencent ts_last=y.qq.com/n/ryqq/album;
+ * @preserve
+ */
+// 内容来自 https://raw.githubusercontent.com/pdone/lx-music-source/main/sixyin/latest.js
